b39df05e5db92e7a2177858fb9ca54e3e011d5dd
Denis Gukov <denguk@gmail.com>
2025-04-21 02:06:58 +0500

    chore: update script

--- deploy/prepare_header_title.sh
+++ deploy/prepare_header_title.sh
@@ -7,9 +7,9 @@ echo "Move HTML files to their own directories and rename them to index.html"
 
 find . -type f -name "index.html" | while read file; do
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/\>Semaphore Docs\</></g' "$file"
+    sed -i '' 's/>Semaphore Docs//g' "$file"
   else
-    sed -i 's/\>Semaphore Docs\</></g' "$file"
+    sed -i 's/>Semaphore Docs/>/g' "$file"
   fi
 done
 
