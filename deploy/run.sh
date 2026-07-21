6e0e7cd068bb513ac80b3360abafbdec21bebd59
Denis Gukov <denguk@gmail.com>
2026-07-21 15:20:33 +0500

    ci: update script

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,15 @@
-# ansible-galaxy collection install -r requirements.yml
-ansible-playbook -i invs/$1/hosts $2.yml --vault-password-file ~/semaphore_pass --extra-vars=@~/semaphore_vars.json -vvvv
+#!/bin/bash
+
+UV_PYTHON="$(uv tool dir)/ansible-core/bin/python"
+
+EXTRA_ARGS=()
+if [ -x "$UV_PYTHON" ]; then
+  EXTRA_ARGS+=(-e ansible_python_interpreter="$UV_PYTHON")
+fi
+
+ansible-playbook \
+  -i invs/$1/hosts $2.yml \
+  --vault-password-file ~/semaphore_pass \
+  --extra-vars=@~/semaphore_vars.json \
+  -vvvv \
+  "${EXTRA_ARGS[@]}"
--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ cd deploy
 if [ -f ~/venv/bin/activate ]; then
     echo "Activating virtual environment"
     source ~/venv/bin/activate
-else
-    echo "Use ansible without virtual environment"
 fi
-yarn build
+
+npm run build
+
 ./run.sh aws deploy
