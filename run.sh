cc21b10149986d9d7dd5157e2c4e5664ecc1bb35
Denis Gukov <denguk@gmail.com>
2026-09-13 16:26:34 +0500

    chore: small fix

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-cd deploy
+cd scripts
+
+./build-parallel.sh
+
+cd ../deploy
 if [ -f ~/venv/bin/activate ]; then
     echo "Activating virtual environment"
     source ~/venv/bin/activate
 fi
-
-# npm run build
-
-# (cd scripts && ./build-parallel.sh)
 
 ./run.sh aws deploy
